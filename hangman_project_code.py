c8fa37ec17b5cf72913e196cbf4bf3ae0e512b40
IgnacyKlimont <ignacyklimont@gmail.com>
2022-08-13 14:03:10 +0200

    Updated description

--- hangman_project_code.py
+++ hangman_project_code.py
@@ -1,13 +1,10 @@
-# PROJECT: HANGMAN GAME
+# Project: Hangman Game
+# Python 3.10
 # DATE: 22.07.2022
-
-
 import random
 
 
 # Class Hangman
-
-
 class Hangman:
 
 	def __init__(self, words, attempts):
@@ -17,9 +14,10 @@ class Hangman:
 		self.won_games = 0
 		self.lost_games = 0
 		self.guesses = set()
+		self.hidden = ''
 
 	def choose(self):
-		choice = input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit: ')
+		choice = input('Type "play" to play the game, "results" to show the scoreboard, and "exit" to quit: \n')
 		if choice == 'play':
 			game.start()
 		elif choice == 'results':
@@ -33,16 +31,15 @@ class Hangman:
 
 	def start(self):
 		while self.attempts > 0:
-			global hidden
-			hidden = ''.join(let if let in self.guesses else '-' for let in self.answer)
-			print(hidden)
-			if '-' not in hidden:
+			self.hidden = ''.join(let if let in self.guesses else '-' for let in self.answer)
+			print(self.hidden)
+			if '-' not in self.hidden:
 				print(f'You guessed the word {self.answer}!')
 				print('You survived!')
 				self.won_games += 1
 				self.answer = random.choice(self.words)
-				hidden = ''
-				self.guesses = set()
+				self.hidden = ''
+				self.guesses.clear()
 				game.choose()
 			guess = input('Input a letter: ')
 			if len(guess) != 1:
@@ -52,20 +49,19 @@ class Hangman:
 			elif guess in self.guesses:
 				print(f"You've already guessed this letter.")
 			elif guess not in self.answer:
-				print(f"That letter doesn't appear in the word. # {self.attempts} attempts")
 				self.attempts -= 1
+				print(f"That letter doesn't appear in the word. # {self.attempts} attempts")
 			self.guesses.add(guess)
 		print("You lost!")
 		self.lost_games += 1
 		self.answer = random.choice(self.words)
-		hidden = ''
-		self.guesses = set()
+		self.hidden = ''
+		self.guesses.clear()
 		game.choose()
 
-# Configuration
-config = {
-	'words': ['python', 'java', 'swift', 'javascript'],
-	'attempts': 8
-}
-game = Hangman(**config)
-game.choose()
+
+# Initializing the game
+if __name__ == '__main__':
+	config = {'words': ['python', 'java', 'swift', 'javascript'], 'attempts': 8}
+	game = Hangman(**config)
+	game.choose()
